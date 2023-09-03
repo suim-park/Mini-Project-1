aef5d480c9ca60b3214c3c53571472075d90b6e4
suim.park <suim.park@duke.edu>
2023-09-03 17:49:59 -0400

    -

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-"""
-Main cli or app entry point
-"""
+# Main File
 
-from mylib.calculator import add
-import click
 
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+def add(a, b):
+    return a + b
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    result = add(1, 2)
+    print("result = ", result)
--- test_main.py
+++ test_main.py
@@ -1,10 +1,9 @@
-"""
-Test goes here
-
-"""
-
-from mylib.calculator import add
+# Test File
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+if __name__ == "__main__":
+    test_add()
